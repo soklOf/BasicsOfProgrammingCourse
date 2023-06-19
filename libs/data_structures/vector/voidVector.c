5990610d7dbe6264d7ded01be433c1949308ad92
soklOf <99805413+soklOf@users.noreply.github.com>
2023-06-19 17:37:27 +0300

    Update voidVector.c

--- libs/data_structures/vector/voidVector.c
+++ libs/data_structures/vector/voidVector.c
@@ -1,1 +1,79 @@
+#include "voidVector.h"
 
+void badAlloc(const int *a) {
+    if (a == NULL) {
+        fprintf(stderr, "bad alloc ");
+        exit(1);
+    }
+}
+
+vectorVoid createVectorV(const size_t n, size_t baseTypeSize) {
+    int *a = (int *) malloc(sizeof(int) * n);
+    badAlloc(a);
+    return (vectorVoid) {a, 0, n, sizeof(baseTypeSize)};
+}
+
+void reserveV(vectorVoid *v, size_t newCapacity) {
+    if (newCapacity) {
+        v->data = (int *) realloc(v->data, sizeof(int) * newCapacity);
+        badAlloc(v->data);
+        v->capacity = newCapacity;
+        if (newCapacity < v->size)
+            v->size = newCapacity;
+    } else
+        deleteVectorV(v);
+}
+
+void shrinkToFitV(vectorVoid *v) {
+    reserveV(v, v->size);
+}
+
+void clearV(vectorVoid *v) {
+    v->size = 0;
+}
+
+void deleteVectorV(vectorVoid *v) {
+    free(v->data);
+    v->data = NULL;
+    v->capacity = 0;
+    v->size = 0;
+}
+
+bool isEmptyV(vectorVoid *v) {
+    return v->size == 0;
+}
+
+bool isFullV(vectorVoid *v) {
+    return v->size == v->capacity;
+}
+
+void getVectorValueV(vectorVoid *v, size_t index, void *destination) {
+    if (index >= v->size)
+        fprintf(stderr, "IndexError: a[%d] is not exists", index);
+    char *source = (char *) v->data + index * v->baseTypeSize;
+    memcpy(destination, source, v->baseTypeSize);
+}
+
+void setVectorValueV(vectorVoid *v, size_t index, void *source) {
+    if (index > v->size)
+        fprintf(stderr, "IndexError: a[%d] is not exists", index);
+    char *destination = (char *) v->data + index * v->baseTypeSize;
+    memcpy(destination, source, v->baseTypeSize);
+
+}
+
+void popBackV(vectorVoid *v) {
+    if (isEmptyV(v)) {
+        fprintf(stderr, "Vector is empty");
+        exit(1);
+    }
+    v->size--;
+}
+
+void pushBackV(vectorVoid *v, void *source) {
+    if (v->capacity == 0)
+        reserveV(v, 1);
+    else if (isFullV(v))
+        reserveV(v, v->capacity * 2);
+    setVectorValueV(v, v->size++, source);
+}
