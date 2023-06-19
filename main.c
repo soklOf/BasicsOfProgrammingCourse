5237e9484a41e5a3f51bef950e3301390ca66888
soklOf <99805413+soklOf@users.noreply.github.com>
2023-06-19 17:42:10 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+#include "libs/data_structures/vector/vector.h"
+#include "libs/data_structures/vector/voidVector.h"
+
 
 int main() {
-    printf("Hello, World!\n");
+    size_t n;
+    scanf("%zd", &n);
+
+    vectorVoid v = createVectorV(0, sizeof(float));
+    for (int i = 0; i < n; i++) {
+        float x;
+        scanf("%f", &x);
+
+        pushBackV(&v, &x);
+    }
+    for (int i = 0; i < n; i++) {
+        float x;
+        getVectorValueV(&v, i, &x);
+
+        printf("%f ", x);
+    }
+
     return 0;
 }
